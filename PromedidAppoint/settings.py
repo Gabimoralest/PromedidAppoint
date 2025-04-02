21891f0bd310699bcd44848b8099c5f2c45d7bd1
Katerine Morales Vasquez <iamkaterinemorales@gmail.com>
2025-04-02 13:04:24 -0500

    Creacion de la app-usuario

--- PromedidAppoint/settings.py
+++ PromedidAppoint/settings.py
@@ -43,7 +43,18 @@ INSTALLED_APPS = [
     'facturacion',
     'sedes',
     'rest_framework',
+    'rest_framework.authtoken'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuario.Usuario'
--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
+
+class Usuario(AbstractUser):
+    ROLES = [
+        ('administrador', 'Administrador'),
+        ('recepcionista', 'Recepcionista'),
+        ('asesor', 'Asesor')
+    ]
+    
+    grupos = models.ManyToManyField(
+        Group,
+        related_name="usuario_grupos",  # Agregamos related_name para evitar el conflicto
+        blank=True
+    )
+    
+    usuario_permisos = models.ManyToManyField(
+        Permission,
+        related_name="usuario_permisos",  # También aquí
+        blank=True
+    )
+    
+    rol = models.CharField(max_length=20, choices=ROLES)
+    
+    def __str__(self):
+        return f'Usuario: {self.username} - Rol: {self.get_rol_display()}'
